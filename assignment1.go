366e626b3f4ee7b47beb7647c9b6cebf7dc3bf4d
raditeo <raditeo@gmail.com>
2022-03-24 05:46:27 +0700

    Update assignment1.go

--- assignment1.go
+++ assignment1.go
@@ -17,7 +17,7 @@ func main() {
 	args := os.Args
 	arg, err := strconv.Atoi(args[1])
 
-	studentList := []Student{
+	studentLists := []Student{
 		{Nama: "Raditeo", Alamat: "Semarang", Pekerjaan: "Pranata Komputer UIN Walisongo", Alasan: "Peningkatan skill pegawai"},
 		{Nama: "Satria", Alamat: "Kendal", Pekerjaan: "Pranta Komputer UIN Walisongo", Alasan: "Peningkatan skill pegawai"},
 		{Nama: "Mubin", Alamat: "Demak", Pekerjaan: "Pegawai BLU UIN Walisongo", Alasan: "Peningkatan skill pegawai"},
@@ -27,7 +27,7 @@ func main() {
 
 	if err == nil {
 		fmt.Println("Siswa nomor urut:", arg)
-		PrintStudent(studentList[arg-1])
+		PrintStudent(studentLists[arg-1])
 	}
 }
 
